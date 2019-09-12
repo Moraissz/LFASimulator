c877540145a1ecdb20350125a83ececb5d546437
Lucas Morais <lucas.moraissz07@gmail.com>
2019-09-12 17:24:00 -0300

    first commit

---
+++ src/trabalho2lfa/Trabalho2LFA.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho2lfa;
+
+import View.MainView;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+
+/**
+ *
+ * @author lucas
+ */
+public class Trabalho2LFA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainView mainView = new MainView();
+        mainView.setVisible(true);
+    }
+ 
+}
